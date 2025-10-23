d2719c7ed4a62d001348fd9efec54e97b0b65f90
“[Hamza <[hamzaelgarrai.dev@gmail.com]>
2025-10-23 11:12:59 +0100

    Challenge Laravel : Mini Gestionnaire de Routes et Contrôleurs

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get('/salut', function () {
+    $nom = "hamza";
+    return "Bonjour . $nom";
+});
+
+Route::get('/accueil',function(){
+    return view('accueil');
+});
+
+Route::get('/test',[TestController::class , "index" ]);
+
+Route::get('/home',[TestController::class , "show" ]);
+
+Route::get('/salutation/{prenom}' , [TestController::class , 'greet']);
+
+Route::get('/profile/{id}/{age}' , [TestController::class ,'profile']);
+
+Route::get('/article/{id}' , [TestController::class ,'showArticle']);
